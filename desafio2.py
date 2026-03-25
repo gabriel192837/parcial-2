6741d04c30aa9bbdece0ab8b5be64fe41a526fb4
gabriel192837 <gabriel.duma.ramos@escola.pr.gov.br>
2026-03-25 08:41:16 -0300

    desafio2.py

--- desafio2.py
+++ desafio2.py
@@ -1,1 +1,8 @@
+# Passo 1: peça o valor ao usuário
+numero = int(input("Insira um número inteiro para a verificação: "))
 
+# Passo 2: confirme o resto da divisão por 2
+if numero % 2 == 0:
+    print(f"O número {numero} é PAR.")
+else:
+    print(f"O número {numero} é ÍMPAR.")
